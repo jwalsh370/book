2b8665a16a685332029c661a0b168c701a17d5c7
jwalsh370 <jwalsh370@gmail.com>
2017-05-12 19:50:48 -0700

    add admin and book component for details and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { AdminComponent } from './admin/admin.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { NewBookComponent } from './new-book/new-book.component';
+import { GenrePipe } from './genre.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    AdminComponent,
+    BookDetailComponent,
+    BookListComponent,
+    NewBookComponent,
+    GenrePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/genre.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'genre'
+})
+export class GenrePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return null;
+  }
+
+}
