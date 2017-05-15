e293fbea13fb47812bbe452bf77ac1e1f2cf3679
jwalsh370 <jwalsh370@gmail.com>
2017-05-15 13:38:59 -0700

    debug genre pipe.

--- src/app/genre.pipe.ts
+++ src/app/genre.pipe.ts
@@ -12,14 +12,72 @@ export class GenrePipe implements PipeTransform {
     var output: Book[] = [];
       if(selectGenre === "allGenres") {
         return input;
-      } else  {
-        for(var i = 0; i < input.length; i++){
-          if(input[i].genre.includes(selectGenre)) {
-            output.push(input[i]);
+      } else if (selectGenre === "comic") {
+          for (var i = 0; i < input.length; i++) {
+            if (input[i].genre === "comic") {
+              output.push(input[i]);
+            }
           }
-        }
-        return output;
-      }
+          return output;
+        }  else if (selectGenre === "history") {
+          for (var i = 0; i < input.length; i++) {
+            if (input[i].genre === "history") {
+              output.push(input[i]);
+            }
+          }
+          return output;
+        } else if (selectGenre === "horror") {
+           for (var i = 0; i < input.length; i++) {
+             if (input[i].genre === "horror") {
+               output.push(input[i]);
+             }
+           }
+           return output;
+         }  else if (selectGenre === "comedy") {
+           for (var i = 0; i < input.length; i++) {
+             if (input[i].genre === "comedy") {
+               output.push(input[i]);
+             }
+           }
+           return output;
+         }  else if (selectGenre === "fantasy") {
+           for (var i = 0; i < input.length; i++) {
+             if (input[i].genre === "fantasy") {
+               output.push(input[i]);
+             }
+           }
+
+           return output;
+         }  else if (selectGenre === "trueCrime") {
+           for (var i = 0; i < input.length; i++) {
+             if (input[i].genre === "trueCrime") {
+               output.push(input[i]);
+             }
+           }
+           return output;
+         }  else if (selectGenre === "sci-fi") {
+           for (var i = 0; i < input.length; i++) {
+             if (input[i].genre === "sci-fi") {
+               output.push(input[i]);
+             }
+           }
+           return output;
+         }  else if (selectGenre === "autobiography") {
+           for (var i = 0; i < input.length; i++) {
+             if (input[i].genre === "autobiography") {
+               output.push(input[i]);
+             }
+           }
+           return output;
+         }  else if (selectGenre === "other") {
+           for (var i = 0; i < input.length; i++) {
+             if (input[i].genre === "other") {
+               output.push(input[i]);
+             }
+           }
+           return output;
+
   }
 
+ }
 }
